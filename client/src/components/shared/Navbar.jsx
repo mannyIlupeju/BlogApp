0ae3f4c185cf3fe82c526279a1058283aaa7c10e
machnmb_ <mannybiz@outlook.com>
2023-02-23 12:42:30 -0600

    update: menu position center

--- client/src/components/shared/Navbar.jsx
+++ client/src/components/shared/Navbar.jsx
@@ -16,12 +16,12 @@ function Navbar({isLogin, setisLogin}) {
   return (
     <>
       <div className="container-flex flex md:flex-row flex-col justify-between bg-zinc-400 text-slate-900 p-5 nav-background">
-        <div className="pl-10 mx-auto font-semibold text-3xl flex">
+        <div className="pl-10 mx-auto font-semibold text-3xl flex justify-center">
           <Link to='/'><img src = '/images/memoryblog.png' className="brandlogo" alt="memory logo"/></Link>
           <Link to='/'>Memory Blog</Link>
         </div>
         <nav>
-          <ul className="flex flex-row container mx-auto gap-10">
+          <ul className="flex flex-row container mx-auto gap-10 justify-center">
             {isLogin ?  <Link to='/blog/form'>Form</Link> : null}
             {isLogin ? <a onClick={logOut} className="cursor-pointer">Logout</a> : <Link to='/blog/signin'>Sign Up/Sign In</Link> }
           </ul>
